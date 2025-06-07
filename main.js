062ccc1527c5746ff539832c4b2d126bdd51f0bd
msi <hachimhachemi@gmail.com>
2025-06-07 18:32:15 +0100

    models added

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(light);
     scene.add(light2);
 
-    const occluder = await loadGLTF('../../assets/models/sparkar-occluder/headOccluder.glb');
+    const occluder = await loadGLTF('./models/sparkar-occluder/headOccluder.glb');
     occluder.scene.scale.set(0.065, 0.065, 0.065);
     occluder.scene.position.set(0, -0.3, 0.15);
     occluder.scene.traverse((o) => {
@@ -58,13 +58,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const occluderAnchor = mindarThree.addAnchor(168);
     occluderAnchor.group.add(occluder.scene);
 
-    const glasses = await loadGLTF('../../assets/models/glasses1/scene.gltf');
+    const glasses = await loadGLTF('./models/glasses1/scene.gltf');
     glasses.scene.scale.set(0.01, 0.01, 0.01);
     glasses.scene.renderOrder = 1;
     const glassesAnchor = mindarThree.addAnchor(168);
     glassesAnchor.group.add(glasses.scene);
 
-    const glasses2 = await loadGLTF('../../assets/models/glasses2/scene.gltf');
+    const glasses2 = await loadGLTF('./models/glasses2/scene.gltf');
     glasses2.scene.rotation.set(0, -Math.PI/2, 0);
     glasses2.scene.position.set(0, -0.3, 0);
     glasses2.scene.scale.set(0.6, 0.6, 0.6);
@@ -72,28 +72,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const glasses2Anchor = mindarThree.addAnchor(168);
     glasses2Anchor.group.add(glasses2.scene);
 
-    const hat1 = await loadGLTF('../../assets/models/hat1/scene.gltf');
+    const hat1 = await loadGLTF('./models/hat1/scene.gltf');
     hat1.scene.position.set(0, 1, -0.5);
     hat1.scene.scale.set(0.35, 0.35, 0.35);
     hat1.scene.renderOrder = 1;
     const hat1Anchor = mindarThree.addAnchor(10);
     hat1Anchor.group.add(hat1.scene);
 
-    const hat2 = await loadGLTF('../../assets/models/hat2/scene.gltf');
+    const hat2 = await loadGLTF('./models/hat2/scene.gltf');
     hat2.scene.position.set(0, -0.2, -0.5);
     hat2.scene.scale.set(0.008, 0.008, 0.008);
     hat2.scene.renderOrder = 1;
     const hat2Anchor = mindarThree.addAnchor(10);
     hat2Anchor.group.add(hat2.scene);
 
-    const earringLeft = await loadGLTF('../../assets/models/earring/scene.gltf');
+    const earringLeft = await loadGLTF('./models/earring/scene.gltf');
     earringLeft.scene.position.set(0, -0.3, -0.3);
     earringLeft.scene.scale.set(0.05, 0.05, 0.05);
     earringLeft.scene.renderOrder = 1;
     const earringLeftAnchor = mindarThree.addAnchor(127);
     earringLeftAnchor.group.add(earringLeft.scene);
 
-    const earringRight = await loadGLTF('../../assets/models/earring/scene.gltf');
+    const earringRight = await loadGLTF('./models/earring/scene.gltf');
     earringRight.scene.position.set(0, -0.3, -0.3);
     earringRight.scene.scale.set(0.05, 0.05, 0.05);
     earringRight.scene.renderOrder = 1;
